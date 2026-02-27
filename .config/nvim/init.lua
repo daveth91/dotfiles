34e52a4b4156809469e092265de445a49990b54c
davet <davet@mailbox.org>
2026-02-27 19:59:22 +0100

    change nvim typescript lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,13 +89,5 @@ vim.cmd('packadd! nohlsearch')
 
 
 -- Language servers
-vim.lsp.enable('tsserver')
-
-vim.lsp.config('tsserver', {
-  cmd = {'typescript-language-server', '--stdio'},
-  filetypes = { 'typescript' },
-  root_dir = vim.fs.root(0, {'package.json', '.git'}),
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
+vim.lsp.enable('ts_ls')
 
